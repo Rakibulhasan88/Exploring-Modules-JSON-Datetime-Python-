2e1b62765f298642ca4598757caf5f245325b2f6
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-16 21:02:27 +0600

    Custom module

---
+++ collection_prac.py
@@ -0,0 +1,4 @@
+import collections
+# print(collections.__doc__)
+
+fruits = ['apple', 'banana', 'apple','Orange']
---
+++ index.py
@@ -0,0 +1,5 @@
+import demo
+from demo import add, mul
+
+print(demo.add(4,5))
+print(demo.mul(4,5))
